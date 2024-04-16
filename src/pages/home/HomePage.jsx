736328b006a2b9e7022c197a9881cf3ce4491284
getwie <f95130687@gmail.com>
2024-04-16 20:50:08 +0800

    HomeAccordion&SubmitDoneOK

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import TopBar from '../../components/TopBar';
 import SideBar from '../../components/SideBar';
 import Modal from '../../components/Modal';
@@ -14,6 +14,7 @@ import Swal from 'sweetalert2';
 import { GrFormAdd } from "react-icons/gr";
 import { MdAddchart } from "react-icons/md";
 import dateFormat from 'dateformat';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';  // 引入Font Awesome圖標
 
 export default function HomePage() {
   const [projectData, setProjectData] = useState([]);
@@ -25,6 +26,9 @@ export default function HomePage() {
   const navigate = useNavigate();
   const [ongoingProjects, setOngoingProjects] = useState([]);
   const [completedProjects, setCompletedProjects] = useState([]);
+  const [doneProjects, setDoneProjects] = useState([]);
+
+  const [activeIndex, setActiveIndex] = useState(0);  // 用於記錄當前打開的Accordion索引
 
 
   const {
@@ -187,8 +191,12 @@ export default function HomePage() {
 
   useEffect(() => {
     if (projectData) {
-      const ongoing = projectData.filter(project => calculateProgress(project.currentStage, project.currentSubStage) < 100);
-      const completed = projectData.filter(project => calculateProgress(project.currentStage, project.currentSubStage) === 100);
+      const ongoing = projectData.filter(project => calculateProgress(project.currentStage, project.currentSubStage) < 75);
+      const completed = projectData.filter(project => calculateProgress(project.currentStage, project.currentSubStage) > 75 && project.ProjectEnd === false);
+      const done = projectData.filter(project => project.ProjectEnd === true);
+
+      // stageEnd=true
+      setDoneProjects(done);
       setOngoingProjects(ongoing);
       setCompletedProjects(completed);
     }
@@ -199,83 +207,132 @@ export default function HomePage() {
       <TopBar />
       <div className='flex flex-col my-10 px-10 md:px-20 py-10 w-full items-center'>
         <div className='flex flex-col w-full md:w-11/12 lg:w-3/4'>
-          <div className='flex justify-start mb-4 mt-2'>
-            <h2 className="text-lg font-bold mr-8 pt-5">進行中</h2>
-            <button
-              onClick={() => setCreateProjectModalOpen(true)}
-              className="flex items-center justify-center bg-[#5BA491] hover:bg-[#5BA491]/80 text-white font-semibold rounded-lg px-6 py-2 shadow-md transition duration-200 ease-in-out transform hover:scale-105 mr-4"
-            >
-              <MdAddchart className="mr-2" /> 建立活動
-            </button>
-            <button
-              onClick={() => setInviteProjectModalOpen(true)}
-              className="flex items-center justify-center bg-[#5BA491] hover:bg-[#5BA491]/80 text-white font-semibold rounded-lg px-6 py-2 shadow-md transition duration-200 ease-in-out transform hover:scale-105"
-            >
-              <MdAddchart className="mr-2" /> 加入活動
-            </button>
-          </div>
-
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-            {ongoingProjects.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((projectItem, index) => (
-              <div key={index} className='bg-white w-96 rounded-lg shadow-lg hover:shadow-lg  p-4 flex flex-col space-y-4 hover:scale-105 transition-transform duration-200 ease-out'>
-                <h3 className='text-xl font-bold text-[#5BA491]'>{projectItem.name}</h3>
-                <p className='text-gray-600 font-semibold'>{projectItem.describe}</p>
-                <div className='text-sm text-gray-500'>指導老師：{projectItem.mentor}</div>
-                <div className='text-sm text-gray-500'>邀請碼：{projectItem.referral_code}</div>
-                <div className='flex justify-between text-sm text-gray-500'>
-                  <span className='flex items-center text-gray-500'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M3 12a9 9 0 110 18 9 9 0 010-18zm9 9a9 9 0 100-18 9 9 0 000 18z" />
-                    </svg>
-                    創建於 {dateFormat(projectItem.createdAt, "yyyy/mm/dd")}
-                  </span>
-                  <span className='flex items-center text-gray-500'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M3 12a9 9 0 110 18 9 9 0 010-18zm9 9a9 9 0 100-18 9 9 0 000 18z" />
-                    </svg>
-                    更新於 {formatRelativeTime(projectItem.updatedAt)}
-                  </span>
-                </div>
-                <Tooltip children={"活動進度"} content={`已完成${calculateProgressPercentage(projectItem.currentStage, projectItem.currentSubStage)}%`}>
-                  <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
-                    <div className='bg-[#5BA491] h-2.5 rounded-full transition-all duration-300 ease-in-out' style={{ width: `${calculateProgress(projectItem.currentStage, projectItem.currentSubStage)}%` }}></div>
-                  </div>
-                </Tooltip>
-                <button className='mt-2 bg-[#5BA491] text-white rounded-lg px-4 py-2 hover:bg-[#5BA491]/80 transition duration-200 ease-in-out font-semibold' onClick={() => navigate(`/project/${projectItem.id}/kanban`)}>查看活動</button>
-              </div>
-            ))}
-          </div>
-          <h2 className="text-lg font-bold mb-4 mt-10">已完成</h2>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            {completedProjects.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((projectItem, index) => (
-              <div key={index} className='bg-gray-300 w-96 rounded-lg shadow hover:shadow-lg  p-4 flex flex-col space-y-4 hover:scale-105 transition-transform duration-200 ease-out'>
-              <div className='flex items-center'>
+          <Accordion
+            index={0}
+            title="進行中活動"
+            activeIndex={activeIndex}
+            setActiveIndex={setActiveIndex}
+          >
+            <div className='flex justify-start mb-4 mt-2'>
+              {/* <h2 className="text-lg font-bold mr-8 pt-5">進行中</h2> */}
+              <button
+                onClick={() => setCreateProjectModalOpen(true)}
+                className="flex items-center justify-center bg-[#5BA491] hover:bg-[#5BA491]/80 text-white font-semibold rounded-lg px-6 py-2 shadow-md transition duration-200 ease-in-out transform hover:scale-105 mr-4"
+              >
+                <MdAddchart className="mr-2" /> 建立活動
+              </button>
+              <button
+                onClick={() => setInviteProjectModalOpen(true)}
+                className="flex items-center justify-center bg-[#5BA491] hover:bg-[#5BA491]/80 text-white font-semibold rounded-lg px-6 py-2 shadow-md transition duration-200 ease-in-out transform hover:scale-105"
+              >
+                <MdAddchart className="mr-2" /> 加入活動
+              </button>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
+              {ongoingProjects.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((projectItem, index) => (
+                <div key={index} className='bg-white w-96 rounded-lg shadow-lg hover:shadow-lg  p-4 flex flex-col space-y-4 hover:scale-105 transition-transform duration-200 ease-out'>
                   <h3 className='text-xl font-bold text-[#5BA491]'>{projectItem.name}</h3>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2 text-[#5BA491]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p className='text-gray-500'>{projectItem.describe}</p>
-                <div className='text-sm text-gray-500'>指導老師：{projectItem.mentor}</div>
-                <div className='text-sm text-gray-500'>邀請碼：{projectItem.referral_code}</div>
-                <div className='flex justify-between text-sm text-gray-500'>
-                  <span className='flex items-center'>
-                    創建於 {dateFormat(projectItem.createdAt, "yyyy/mm/dd")}
-                  </span>
-                  <span className='flex items-center'>
-                    更新於 {formatRelativeTime(projectItem.updatedAt)}
-                  </span>
-                </div>
-                <Tooltip children={"活動進度"} content={`已完成${calculateProgressPercentage(projectItem.currentStage, projectItem.currentSubStage)}%`}>
-                  <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
-                    <div className='bg-[#5BA491] h-2.5 rounded-full transition-all duration-300 ease-in-out' style={{ width: '100%' }}></div>
+                  <p className='text-gray-600 font-semibold'>{projectItem.describe}</p>
+                  <div className='text-sm text-gray-500'>指導老師：{projectItem.mentor}</div>
+                  <div className='text-sm text-gray-500'>邀請碼：{projectItem.referral_code}</div>
+                  <div className='flex justify-between text-sm text-gray-500'>
+                    <span className='flex items-center text-gray-500'>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M3 12a9 9 0 110 18 9 9 0 010-18zm9 9a9 9 0 100-18 9 9 0 000 18z" />
+                      </svg>
+                      創建於 {dateFormat(projectItem.createdAt, "yyyy/mm/dd")}
+                    </span>
+                    <span className='flex items-center text-gray-500'>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M3 12a9 9 0 110 18 9 9 0 010-18zm9 9a9 9 0 100-18 9 9 0 000 18z" />
+                      </svg>
+                      更新於 {formatRelativeTime(projectItem.updatedAt)}
+                    </span>
                   </div>
-                </Tooltip>
-                <button className='mt-2 bg-[#5BA491] text-white rounded-lg px-4 py-2 hover:bg-[#5BA491]/80 transition duration-200 ease-in-out font-semibold' onClick={() => navigate(`/project/${projectItem.id}/kanban`)}>製作學習歷程</button>
-              </div>
-            ))}
-          </div>
+                  <Tooltip children={"活動進度"} content={`已完成${calculateProgressPercentage(projectItem.currentStage, projectItem.currentSubStage)}%`}>
+                    <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
+                      <div className='bg-[#5BA491] h-2.5 rounded-full transition-all duration-300 ease-in-out' style={{ width: `${calculateProgress(projectItem.currentStage, projectItem.currentSubStage)}%` }}></div>
+                    </div>
+                  </Tooltip>
+                  <button className='mt-2 bg-[#5BA491] text-white rounded-lg px-4 py-2 hover:bg-[#5BA491]/80 transition duration-200 ease-in-out font-semibold' onClick={() => navigate(`/project/${projectItem.id}/kanban`)}>查看活動</button>
+                </div>
+              ))}
+            </div>
+          </Accordion>
 
+          <Accordion
+            index={1}
+            title="已結束活動"
+            activeIndex={activeIndex}
+            setActiveIndex={setActiveIndex}
+          >
+            {/* <h2 className="text-lg font-bold mb-4 mt-10">已完成</h2> */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              {completedProjects.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((projectItem, index) => (
+                <div key={index} className='bg-gray-300 w-96 rounded-lg shadow hover:shadow-lg  p-4 flex flex-col space-y-4 hover:scale-105 transition-transform duration-200 ease-out'>
+                  <div className='flex items-center'>
+                    <h3 className='text-xl font-bold text-[#5BA491]'>{projectItem.name}</h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2 text-[#5BA491]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p className='text-gray-500'>{projectItem.describe}</p>
+                  <div className='text-sm text-gray-500'>指導老師：{projectItem.mentor}</div>
+                  <div className='text-sm text-gray-500'>邀請碼：{projectItem.referral_code}</div>
+                  <div className='flex justify-between text-sm text-gray-500'>
+                    <span className='flex items-center'>
+                      創建於 {dateFormat(projectItem.createdAt, "yyyy/mm/dd")}
+                    </span>
+                    <span className='flex items-center'>
+                      更新於 {formatRelativeTime(projectItem.updatedAt)}
+                    </span>
+                  </div>
+                  <Tooltip children={"活動進度"} content={`已完成${calculateProgressPercentage(projectItem.currentStage, projectItem.currentSubStage)}%`}>
+                  <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
+                      <div className='bg-[#5BA491] h-2.5 rounded-full transition-all duration-300 ease-in-out' style={{ width: `${calculateProgress(projectItem.currentStage, projectItem.currentSubStage)}%` }}></div>
+                    </div>
+                  </Tooltip>
+                  <button className='mt-2 bg-[#5BA491] text-white rounded-lg px-4 py-2 hover:bg-[#5BA491]/80 transition duration-200 ease-in-out font-semibold' onClick={() => navigate(`/project/${projectItem.id}/kanban`)}>製作學習歷程</button>
+                </div>
+              ))}
+            </div>
+          </Accordion>
+          <Accordion
+            index={2}
+            title="已完成歷程"
+            activeIndex={activeIndex}
+            setActiveIndex={setActiveIndex}
+          >
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              {doneProjects.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((projectItem, index) => (
+                <div key={index} className='bg-gray-300 w-96 rounded-lg shadow hover:shadow-lg  p-4 flex flex-col space-y-4 hover:scale-105 transition-transform duration-200 ease-out'>
+                  <div className='flex items-center'>
+                    <h3 className='text-xl font-bold text-[#5BA491]'>{projectItem.name}</h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2 text-[#5BA491]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p className='text-gray-500'>{projectItem.describe}</p>
+                  <div className='text-sm text-gray-500'>指導老師：{projectItem.mentor}</div>
+                  <div className='text-sm text-gray-500'>邀請碼：{projectItem.referral_code}</div>
+                  <div className='flex justify-between text-sm text-gray-500'>
+                    <span className='flex items-center'>
+                      創建於 {dateFormat(projectItem.createdAt, "yyyy/mm/dd")}
+                    </span>
+                    <span className='flex items-center'>
+                      更新於 {formatRelativeTime(projectItem.updatedAt)}
+                    </span>
+                  </div>
+                  <Tooltip children={"活動進度"} content={`已完成${calculateProgressPercentage(projectItem.currentStage, projectItem.currentSubStage)}%`}>
+                    <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
+                      <div className='bg-[#5BA491] h-2.5 rounded-full transition-all duration-300 ease-in-out' style={{ width: '100%' }}></div>
+                    </div>
+                  </Tooltip>
+                  <button className='mt-2 bg-[#5BA491] text-white rounded-lg px-4 py-2 hover:bg-[#5BA491]/80 transition duration-200 ease-in-out font-semibold' onClick={() => navigate(`/project/${projectItem.id}/kanban`)}>製作學習歷程</button>
+                </div>
+              ))}
+            </div>
+          </Accordion>
         </div>
       </div>
 
@@ -356,4 +413,55 @@ export default function HomePage() {
     </div>
   )
 }
+
+
+const Accordion = ({ index, title, children, activeIndex, setActiveIndex }) => {
+  const [height, setHeight] = useState(0);
+  const contentRef = useRef(null);
+  const isActive = index === activeIndex;
+
+  // 在組件渲染後和isActive變化時執行
+  useEffect(() => {
+    if (isActive && contentRef.current) {
+      // 確保在DOM元素完全載入後設置高度
+      const timer = setTimeout(() => {
+        setHeight(contentRef.current.scrollHeight);
+      }, 50);  // 延遲50毫秒以確保所有內容已經渲染
+      return () => clearTimeout(timer);
+    } else {
+      setHeight(0);
+    }
+  }, [isActive, children]);  // 依賴children有變化時也重新計算高度
+
+  const handleToggle = () => {
+    setActiveIndex(isActive ? null : index);
+    // 切換時立即更新高度
+    if (!isActive && contentRef.current) {
+      setHeight(contentRef.current.scrollHeight);
+    } else {
+      setHeight(0);
+    }
+  };
+
+  return (
+    <div className="">
+      <button
+        className="flex justify-between items-center w-full py-2 px-4 bg-gray-200 rounded-lg shadow hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:bg-gray-300 transition duration-300"
+        onClick={handleToggle}
+      >
+        <span className="font-semibold">{title}</span>
+        {isActive ? <FaChevronUp className="w-3 h-3" /> : <FaChevronDown className="w-3 h-3" />}
+      </button>
+      <div
+        ref={contentRef}
+        style={{ height: isActive ? `${height}px` : "0px", overflow: 'hidden' }}
+        className="transition-height bg-customgreen/5 duration-500 ease-in-out my-1  "
+      >
+        <div className="text-left px-2 py-2">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
 
